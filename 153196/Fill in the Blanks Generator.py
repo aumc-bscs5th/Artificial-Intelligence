f20914cad8b184e88224996c9d329055e0dec8c1
Ahsan Javed <iahsanjaved@yahoo.com>
2018-01-30 19:07:51 +0500

    Update Fill in the Blanks Generator.py

--- 153196/Fill in the Blanks Generator.py
+++ 153196/Fill in the Blanks Generator.py
@@ -1,59 +1,75 @@
 import nltk
 
-fileName = raw_input('Enter the file name: ')
 
-file = open(fileName, "r")
-
-paragraph = ''
-if file.mode == "r":
+def readFile(fileName):
+    paragraph = ''
+    file = open(fileName, "r")
     paragraph = file.read()
-file.close()
+    file.close()
+    return paragraph
+
+def tokenization(paragraph):
+    sents = nltk.sent_tokenize(paragraph)
+    words = [nltk.word_tokenize(sent) for sent in sents]
+    return sents, words
+
+def posTagging(words):
+    posWords = [nltk.pos_tag(word) for word in words]
+    return posWords
+
+
+fileName = raw_input('Enter the file name: ')
 
 # -------------Read File-------------
 print '---------------------------------------------------'
 print 'Paragraph'
 print '---------------------------------------------------'
+paragraph = readFile(fileName)
 print paragraph
 
 
 # Step-1--------Tokenization---------
-
-sents = nltk.sent_tokenize(paragraph)
-copySents = sents
-words = [nltk.word_tokenize(sent) for sent in sents]
-
+sents, words = tokenization(paragraph)
 
 
 # Step-2--------POS Tagging---------
+posWords = posTagging(words)
 
-posWords = [nltk.pos_tag(word) for word in words]
+# Step-3--------Printing POS--------
 print '\n---------------------------------------------------'
 print 'Part of Speech'
 print '---------------------------------------------------'
-
 print posWords
 
 
-
+# Step-4--------Printing Nouns--------
 print '\n---------------------------------------------------'
 print 'Print Nouns'
 print '---------------------------------------------------'
-i = 0
 for posWord in posWords:
     for x in posWord:
         if (x[1] == 'NN'):
-            copySents[i] = copySents[i].replace(x[0], '__________')
             print x[0] + '=> NOUN'
-    i = i + 1
     print '\n'
 
+# Step-5--------Replace Nouns with '____'--------
+i = 0
+fillSents = sents
+answers = []
+for posWord in posWords:
+    for x in posWord:
+        if (x[1] == 'NN'):
+            fillSents[i] = fillSents[i].replace(x[0], '__________')
+            answers.append(x[0])
+    i = i + 1
 
+# Step-6--------Printing Fill in the blanks--------
 print '---------------------------------------------------'
 print 'Fill in the blanks'
 print '---------------------------------------------------'
 i = 1
-for copySent in copySents:
-    print str(i) +'. '+copySent
+for fillSent in fillSents:
+    print str(i) + '. ' + fillSent
     print '\n'
     i = i + 1
 
