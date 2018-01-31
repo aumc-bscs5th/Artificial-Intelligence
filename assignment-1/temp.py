d289a3c9164241485292d4eefda3b498ddc13bfc
Suheer Tahir <31886704+iSuheertahir@users.noreply.github.com>
2018-01-31 23:54:25 +0500

    153158-Fill in the Blanks
    Artificial Intelligence- Fill in the Blanks

---
+++ assignment-1/temp.py
@@ -0,0 +1,71 @@
+import nltk
+def readFile(fileName):
+    paragraph = ''
+    file = open(fileName, "r")
+    paragraph = file.read()
+    file.close()
+    return paragraph
+
+def tokenization(paragraph):
+    sents = nltk.sent_tokenize(paragraph)
+    words = [nltk.word_tokenize(sent) for sent in sents]
+    return sents, words
+
+def posTagging(words):
+    posWords = [nltk.pos_tag(word) for word in words]
+    return posWords
+
+fileName = raw_input('Enter the file name: ')
+
+# Step-1-------Read File-------------
+print '---------------------------------------------------'
+print 'Paragraph'
+print '---------------------------------------------------'
+paragraph = readFile(fileName)
+print paragraph
+
+
+# Step-2--------Tokenization---------
+sents, words = tokenization(paragraph)
+
+
+# Step-3--------POS Tagging---------
+posWords = posTagging(words)
+
+# Step-4--------Printing POS--------
+print '\n---------------------------------------------------'
+print 'Part of Speech'
+print '---------------------------------------------------'
+print posWords
+
+
+# Step-5--------Printing Nouns--------
+print '\n---------------------------------------------------'
+print 'Print Nouns'
+print '---------------------------------------------------'
+for posWord in posWords:
+    for x in posWord:
+        if (x[1] == 'NN'):
+            print x[0] + '=> NOUN'
+    print '\n'
+
+# Step-6--------Replace Nouns with '____'--------
+i = 0
+fillSents = sents
+answers = []
+for posWord in posWords:
+    for x in posWord:
+        if (x[1] == 'NN'):
+            fillSents[i] = fillSents[i].replace(x[0], '__________')
+            answers.append(x[0])
+    i = i + 1
+
+# Step-7--------Printing Fill in the blanks--------
+print '---------------------------------------------------'
+print 'Fill in the blanks'
+print '---------------------------------------------------'
+i = 1
+for fillSent in fillSents:
+    print str(i) + '. ' + fillSent
+    print '\n'
+    i = i + 1
